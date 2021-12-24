ddc89011cf5a6093f57b9aae7f28d9ad1586c900
Michiel Borkent <michielborkent@gmail.com>
2021-12-24 11:17:40 +0100

    common tasks

---
+++ src/common/tasks.clj
@@ -0,0 +1,20 @@
+(ns common
+  (:require [babashka.pods :as pods]
+            [babashka.tasks :refer [clojure]]))
+
+(defn clj-kondo []
+  (pods/load-pod "clj-kondo")
+  (require 'pod.borkdude.clj-kondo)
+  (let [results (eval '(let [src (-> (slurp "project.edn")
+                                     edn/read-string
+                                     (:src-dirs ["src"]))]
+                         (-> (pod.borkdude.clj-kondo/run! {:lint src})
+                             (doto pod.borkdude.clj-kondo/print!))))]
+    (when (-> results :findings seq)
+      (System/exit 1))))
+
+(defn antq [& args]
+  (apply clojure "-Sdeps"
+         (pr-str '{:deps {com.github.liquidz/antq {:mvn/version "1.3.0"}}})
+         "-M" "-m" "antq.core"
+         args))
